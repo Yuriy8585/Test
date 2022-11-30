e64bb3ad9ff1b9dae687c8c0670f979ad033c97b
Yuriy Krasnov <hardy85@mail.ru>
2022-11-30 13:54:12 +0300

    git

---
+++ task0/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Написать программу которая на вход принимает число и выдает его квадрат (число умноженное на само себя)
+//Например
+//4->16
+//-3->9
+//-7->49
+
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+int sqrt = number * number;
+Console.WriteLine("Квадрат числа = " + sqrt);
---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое чило квадратом второго
+// a = 25, b = 5 -> yes
+// a = 2, b = 10 -> no
+// a = 9, b = -3 -> yes
+// a = -3, b = 9 -> no
+
+int a = 25;
+int b = 5;
+if (a == b*b)
+{
+    Console.Write("YES");
+
+}
+else
+{
+    Console.Write("NO");
+}
+
