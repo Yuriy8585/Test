6a5a616d9dcbb1b48d65419636a15217bc072865
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-11-30 14:19:45 +0300

    new task

---
+++ task2/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Напишите программу, которая будет выдавать дни недели
+// a = 1 -> понедельник
+// a = 2 -> вторник
+// a = 3 -> среда
+// a = 4 -> четверг
+// a = 5 -> пятница
+// a = 6 -> суббота
+// a = 7 -> Воскресенье
+
+Console.Write("Enter Number week a = ");
+
+int a = int.Parse(Console.ReadLine());
+if (a == 1)
+{
+    Console.Write("Monday");
+
+}
+
+if (a == 2)
+{
+    Console.Write("Tuesday");
+
+}
+if (a == 3)
+{
+    Console.Write("Wednesday");
+
+}
+if (a == 4)
+{
+    Console.Write("Thursday");
+
+}
+if (a == 5)
+{
+    Console.Write("Friday");
+
+}
+if (a == 6)
+{
+    Console.Write("Saturday");
+
+}
+if (a == 7)
+{
+    Console.Write("Sunday");
+
+}
+else 
+{
+    Console.Write("Not Found");
+}
