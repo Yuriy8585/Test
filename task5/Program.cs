9d5f6291997f1e3dea21cb8c5aa48551470f481c
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-11-30 16:14:00 +0300

    Task5

---
+++ task5/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напиите программу которая на вход принимает одно число N, а выводит значения все целые числа от -N до N
+
+Console.Write("Enter int N = ");
+Int N = int.Parse(Console.ReadLine());
+Int a = -N;
+while (a < N+1)
+{
+    Console.Write(" " + a + " ");
+    a = a + 1;
+}
