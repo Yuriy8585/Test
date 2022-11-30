364232f7d21f22844ba482548cd379dac2241a49
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-11-30 16:48:34 +0300

    task7

--- task7/Program.cs
+++ task7/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//напишите программу которая принимает на вход трехзначное число а на выходе показывает последнюю цифру этого числа
+//456 -> 6
+//782 -> 2
+
+Console.Write("Enter int N = ");
+int N = int.Parse(Console.ReadLine());
+
+int a = 0;
+double b = 0;
+
+a = N%10;
+b = N/10;
+
+Console.WriteLine("Cut" + " " + a);
+Console.Write("Previous Numbers" + " " + b);
+
+
