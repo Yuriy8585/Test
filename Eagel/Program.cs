983cada95dc57d62df01d7d9d18739c81b3913f3
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-12-07 17:02:35 +0300

    mistakes

--- Eagel/Program.cs
+++ Eagel/Program.cs
@@ -1,57 +1,29 @@
 ﻿// Орел
 Console.Clear();
 
-Random rnd = new Random();
-            int a = 0;
-            int b = 0;
-            int c = 100;
- 
-            Console.WriteLine("Орел или Решка");
-            while (true)
-            {
- 
-                Console.WriteLine("Введите число");
-                int i1 = rnd.Next(0, 2);
-                    string i = Console.ReadLine();
-                
-                while (true)
-                {
-                    if (i == "1" || i == "0")
-                    {
-                        break;
-                    }
-                    else if(i == "Результат")
-                    {
-                        c = c / (a + b) * a ;
-                        Console.WriteLine($"Побед {a}. Поражений {b}. Ваша удача:{c}%");
-                        Console.WriteLine("Введите число");
-                        i = Console.ReadLine();
-                        if (i == "0" || i == "1")
-                        {
-                          
-                            break;
-                        }
-                       
-                    }
-                     else
-                    {
-                        Console.WriteLine("Введите 0 или 1");
-                        i = Console.ReadLine();
-                    }
-                }
-                if (i1 == Convert.ToInt32(i))
-                {
-                    Console.WriteLine("Орел");
-                    a++;
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Решка");
-                    b++;
-                    
-                    break;
-                        
-                }
-                
-            }
+int count = 0;
+int countOrel = 0;
+int countReshka = 0;
+
+while(true)
+{
+    int num = new Random().Next(0, 2);
+    if (num == 0)
+    {
+        Console.WriteLine("OREL");
+        countOrel++;
+        countReshka = 0;
+    }
+    else
+    {
+        Console.WriteLine("RESHKA");
+        countReshka++;
+        countOrel = 0;
+    }
+    count++;
+    if (countOrel == 3 || countReshka == 3)
+    {
+        Console.Write($"Made {count} try");
+        break;
+    }
+}
