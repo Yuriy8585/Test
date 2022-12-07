8cdb07215bcae86315236397e0bde60fb8348930
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-12-07 11:10:15 +0300

    pall

---
+++ Pall/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая на вход принимает слово и проверяет, является ли оно палиндромом
+
+Console.Clear();
+Console.WriteLine("Enter WORD = ");
+
+string text = Console.ReadLine();
+string newText = "";
+
+int len = text.Length;
+
+for (int i = 0; i < len; i++)
+{
+    bool check = newText.Contains(text[i]);
+    if (!check)
+    {
+        newText += text[i];
+    }
+    else
+    {
+        string elem = (text[i]).ToString();
+        newText = newText.Replace(elem, "");
+    }
+}
+
+if (newText.Lenght < 2) 
+{ 
+    Console.WriteLine("PALL");
+}
+else 
+{
+    Console.WriteLine("NOT PALL");
+}
